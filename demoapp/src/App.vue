5e5a3c6f795847851d1794278714081bf00be661
bestchanel <samsunga77789@gmail.com>
2024-12-07 19:48:07 +0700

    Finished_Learning

---
+++ demoapp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- MENU -->
+  <MenuComponent />
+
+  <router-view/>
+
+  <!-- Footer -->
+  <FooterComponent />
+
+</template>
+
+<script>
+
+  import MenuComponent from '@/components/MenuComponent.vue'
+  import FooterComponent from '@/components/FooterComponent.vue'
+
+  export default {
+    components: {
+      MenuComponent,
+      FooterComponent
+    }
+  }
+</script>
